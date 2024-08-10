708adbe01e077bfa47bd8061077c06cb2fa0b0a6
Douglas Rodrigues <douglasrodriguesconectadoifmg@gmail.com>
2024-08-10 15:19:22 -0300

    hoisting

---
+++ javascript/02-hoisting/index.js
@@ -0,0 +1,12 @@
+//var pais; // declaração - ela move pro topo
+//pais = 'brasil'; // atribuição - ela fica no lugar onde foi aribuida
+
+//var pais = 'Brasil';
+
+//console.log(pais);
+//var pais = 'Brasil'; 
+
+
+console.log(pais);
+let = pais = 'Brasil'
+//console.log(pais);
