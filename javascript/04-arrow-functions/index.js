db1d4fd7ad9b063fa46a26aa270c9f9791c4b0d5
Douglas Rodrigues <douglasrodriguesconectadoifmg@gmail.com>
2024-08-11 12:21:40 -0300

    arrow-functions

---
+++ javascript/04-arrow-functions/index.js
@@ -0,0 +1,14 @@
+let resultado = (numero1, numero2)=>{
+    return numero1 + numero2
+}
+console.log(resultado(12,5))
+/*------------------------------------*/
+
+const incentivarQuester = nomeQuester => {
+    console.log('Parabens ' + nomeQuester + ', voçê chegou ao módulo de js intermediario')
+}
+incentivarQuester('Roberto');
+/*------------------------------------------*/
+
+let resultadoDaSoma = (numero1, numero2) => numero1 + numero2
+console.log(resultadoDaSoma(12,5));
