f27211bf5f075388d893d32664d57935e557521f
Douglas Rodrigues <douglasrodriguesconectadoifmg@gmail.com>
2024-08-26 22:42:25 -0300

    trabalhandoArrays

--- javascript/08-trabalhando-com-arrays/index.js
+++ javascript/08-trabalhando-com-arrays/index.js
@@ -1,6 +1,27 @@
 let frutas = ['banana', 'maçã', 'laranja','pêra']
 let frutasTamanho = frutas.length
 
-for(let i = 0; i < frutasTamanho; i++){
-    console.log('frutas: ' + frutas[i]);
-}
+//for(let i = 0; i < frutasTamanho; i++){
+  //  console.log('frutas: ' + frutas[i]);
+//}
+//frutas.forEach(function(item){
+  //  console.log('Frutas: ' + item)
+//})
+frutas.push('manga')
+console.log(frutas);
+
+frutas.pop();
+console.log(frutas)
+
+frutas.shift();
+console.log(frutas)
+let posicaolaranjas = frutas.indexOf
+('laranja');
+console.log(posicaolaranjas);
+
+frutas.unshift('Morango');
+console.log(frutas)
+
+let posicaolaranja = frutas.indexOf
+('laranja');
+console.log(posicaolaranja);
