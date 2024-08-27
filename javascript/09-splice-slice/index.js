cdeb69cc5976dfc7ef31b957819747a29beed092
Douglas Rodrigues <douglasrodriguesconectadoifmg@gmail.com>
2024-08-26 23:19:51 -0300

    splice e slice

---
+++ javascript/09-splice-slice/index.js
@@ -0,0 +1,15 @@
+let frutas = ['Banana','Maça','Larana','Uva'];
+
+//let frutasExtraidas = frutas.slice(1,3);
+
+//console.log(frutasExtraidas);
+
+//console.log(frutas);
+
+//let frutasextraidas = frutas.splice(1,2);
+//console.log(frutasextraidas);
+//console.log(frutas);
+
+let removeEadicionaFrutas = frutas.splice(1,2 , "Goiaba", "abacaxi","pera");
+console.log(frutas);
+console.log(removeEadicionaFrutas);
