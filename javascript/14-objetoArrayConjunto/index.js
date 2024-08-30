beadce20fde6c396231570cdb19667c1fe9411b3
Douglas Rodrigues <douglasrodriguesconectadoifmg@gmail.com>
2024-08-29 22:49:04 -0300

    array em conjunto com OB

--- javascript/14-objetoArrayConjunto/index.js
+++ javascript/14-objetoArrayConjunto/index.js
@@ -13,3 +13,19 @@ let cliente = {
 console.log(cliente.ultimoPedido.jogos[0].nome)
 console.log(cliente.ultimoPedido.jogos[1].classificacao)
 console.log(cliente);
+
+let videoGame = {
+    nome: 'xbox',
+    valor: 3000,
+    jogos: [
+        {nome: 'final fantasy'},
+        {nome: 'fallout'}
+    ]
+}
+
+let jogos3 = {
+    nome: 'fifa',
+    classificacao: 'mais 18'
+}
+videoGame.jogos.push(jogos3);
+console.log(videoGame);
