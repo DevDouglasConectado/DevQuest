6692fe231765f2f9d42883ce7f4f505e4fc67628
Douglas Rodrigues <douglasrodriguesconectadoifmg@gmail.com>
2024-08-31 22:33:57 -0300

    manipulandoDom

---
+++ javascript/15-dom/index.js
@@ -0,0 +1,13 @@
+console.log(document);
+
+let p = document.getElementById('paragrafo1');
+console.log(p);
+
+let inputPorClasse = document.getElementsByClassName('tag-de-input');
+console.log(tag-de-input[0]);
+
+let inputPorTag = document.getElementsByTagName('h1');
+console.log(inputPorTag);
+
+let inputPorName = document.querySelectorAll("input[name='email']");
+console.log(inputPorName[1]);
