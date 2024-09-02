1c2c7ea6a0a6631a53946344d09e27149b624d7e
Douglas Rodrigues <douglasrodriguesconectadoifmg@gmail.com>
2024-09-01 23:11:27 -0300

    manipulando o dom

--- javascript/17-manipulando-o-Dom/index.js
+++ javascript/17-manipulando-o-Dom/index.js
@@ -1,9 +1,11 @@
+
 function adicionarComentario(){
     let inputComentario = document.getElementsByName('novo-comentario');
 
     let textoDigitado = inputComentario[0].value;
     
-    let novosComentarios = document.getElementById('novos-comentarios');
+    /* id da div */
+    let novosComentarios = document.getElementById('inserirTextos');
 
     novosComentarios.innerHTML = novosComentarios.innerHTML + 
     `<p>Novo comentário ${textoDigitado}</p>`
